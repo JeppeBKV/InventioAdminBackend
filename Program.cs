9d88953f1df992580ad118809cc8ef07d2ea3159
Jeppe Vad <jva@inventio.it>
2023-11-20 14:07:26 +0100

    ready for test db conn

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.Azure.Cosmos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+Settings.CosmosDbUrl = builder.Configuration.GetSection("AzureCosmosDbSettings").GetValue<string>("URL");
+Settings.CosmosDbKey = builder.Configuration.GetSection("AzureCosmosDbSettings").GetValue<string>("PrimaryKey");
+Settings.CosmosDbName = builder.Configuration.GetSection("AzureCosmosDbSettings").GetValue<string>("DatabaseName");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -10,12 +16,12 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+// if (app.Environment.IsDevelopment())
+// {
+app.UseSwagger();
+app.UseSwaggerUI();
+// }
+app.MapGet("/", () => "Connected");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
@@ -23,3 +29,14 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public class Settings
+{
+    public static string CosmosDbUrl = "";
+    public static string CosmosDbKey = "";
+    public static string CosmosDbName = "";
+}
+static public class InventioAdminCosmosDB
+{
+    static public CosmosClient client = new (Settings.CosmosDbUrl, Settings.CosmosDbKey);
+}
